a752072e1e0551a88e0d2d805013c073ab57f018
arnab <arnavchattoraj@gmail.com>
2021-05-22 15:32:39 +0530

    The name of the main class has been changed

--- springrest/src/main/java/com/mindtree/springrest/SpringrestApplicationMain.java
+++ springrest/src/main/java/com/mindtree/springrest/SpringrestApplicationMain.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringrestApplication {
+public class SpringrestApplicationMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringrestApplication.class, args);
+		SpringApplication.run(SpringrestApplicationMain.class, args);
 		// This is the change made to change a file and commit and push/commit to the repository
 		System.out.println("Changes made to Spring rest application");
 	}
