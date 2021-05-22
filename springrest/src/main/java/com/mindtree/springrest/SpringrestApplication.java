aa96d7178c42645a7088e73176f2b21f6fb4f82e
arnab <arnavchattoraj@gmail.com>
2021-05-22 15:26:52 +0530

    Just modified the main method of the application

--- springrest/src/main/java/com/mindtree/springrest/SpringrestApplication.java
+++ springrest/src/main/java/com/mindtree/springrest/SpringrestApplication.java
@@ -8,6 +8,8 @@ public class SpringrestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestApplication.class, args);
+		// This is the change made to change a file and commit and push/commit to the repository
+		System.out.println("Changes made to Spring rest application");
 	}
 
 }
